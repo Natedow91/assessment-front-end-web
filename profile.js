d730bd51ca941fb168b44939f45f726b6e8faaf3
nate Dowdle <nathan.t.dowdle@gmail.com>
2022-02-25 12:20:28 -0700

    Done with Assesment

---
+++ profile.js
@@ -0,0 +1,14 @@
+const element = document.getElementById("color");
+
+element.addEventListener("click", function(){ alert("Favorite Color Green")
+})
+
+const place = document.getElementById("place");
+
+place.addEventListener("click", function(){ alert("Favorite place: Limerick Ireland")
+})
+
+const ritual = document.getElementById("ritual");
+
+ritual.addEventListener("click", function(){ alert("Favorite Ritual: La Santa Cena")
+})
